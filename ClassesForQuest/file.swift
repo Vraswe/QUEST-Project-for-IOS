8fcae89786b75d05212b90b5f69d56bddd138eb7
Vraswe <40007077+Vraswe@users.noreply.github.com>
2019-05-26 16:09:03 -0700

    Create file.swift

---
+++ ClassesForQuest/file.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  QUEST
+//
+//  Created by  Black_diamond on 26/05/2019.
+//  Copyright © 2019 Паша. All rights reserved.
+//
+
+import Foundation
+struct TitleData {
+    let name: String
+    let descr: String
+    let pic1: String?
+    let pic2: String?
+    let pic3: String?
+    let pic4: String?
+    
+}
